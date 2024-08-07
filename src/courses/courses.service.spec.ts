8ded5a30f593a24597f1e5b24642ecd473f1fe34
Gabriel Silva <gabiles278@gmail.com>
2024-08-07 10:38:38 -0300

    test<CourseService>: add test for create course

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,21 +1,62 @@
+import { randomUUID } from 'node:crypto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './dto/create-course.dto';
 import { CoursesRepository } from './repositories/courses.repository';
 
-describe('CoursesService', () => {
+const createCourseDto: CreateCourseDto = {
+	name: 'Test',
+	description: 'test description',
+	tags: [{ name: 'nestjs' }],
+};
+
+describe('CoursesService unit tests', () => {
 	let service: CoursesService;
+	let id: string;
+	let created_at: Date;
+	let expectOutputTags: [{ id: string; name: string }];
+	let expectOutputCourses: any;
+	let mockCoursesRepository: any;
 
 	beforeEach(async () => {
-		const module: TestingModule = await Test.createTestingModule({
-			providers: [CoursesService, CoursesRepository],
-		}).compile();
+		id = randomUUID();
+		created_at = new Date();
+		expectOutputTags = [
+			{
+				id,
+				name: 'nestjs',
+			},
+		];
+		expectOutputCourses = {
+			id,
+			name: 'Test',
+			description: 'test description',
+			created_at,
+			tags: expectOutputTags,
+		};
 
-		service = module.get<CoursesService>(CoursesService);
+		mockCoursesRepository = {
+			save: vi.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+			findOneById: vi
+				.fn()
+				.mockReturnValue(Promise.resolve(expectOutputCourses)),
+			findAll: vi.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+			delete: vi.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+			updateCourse: vi
+				.fn()
+				.mockReturnValue(Promise.resolve(expectOutputCourses)),
+		};
+		service = new CoursesService(mockCoursesRepository);
 	});
 
 	it('should be defined', () => {
-		console.log(service);
-
 		expect(service).toBeDefined();
+	});
+
+	it('should create course', async () => {
+		const sut = await service.create(createCourseDto);
+
+		expect(sut).toEqual(expectOutputCourses);
+		expect(mockCoursesRepository.save).toHaveBeenCalled();
 	});
 });
